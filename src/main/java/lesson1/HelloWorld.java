4d91d0943d5e8ad796517e58f79d8bf046aebd0e
Pavel <54819066+Fischerski1@users.noreply.github.com>
2020-09-14 13:38:23 +0300

    + task1

---
+++ src/main/java/lesson1/HelloWorld.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+/**
+ * @author Pavel Trofimov
+ * @code Hello World
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
